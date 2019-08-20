1ea51763be18bdc1b7779a9ceb3a07431049c4de
Liam Bindle <liambindle@gmail.com>
2019-08-20 17:51:14 -0300

    different -convert for GCC and GCHP

--- CMakeScripts/GC-DefaultCompilerOptions.cmake
+++ CMakeScripts/GC-DefaultCompilerOptions.cmake
@@ -1,13 +1,18 @@
 if("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "Intel")
     target_compile_options(BaseTarget INTERFACE
-        -cpp -w -auto -noalign -convert big_endian -fp-model source -mcmodel=medium
+        -cpp -w -auto -noalign
+        $<$<BOOL:"${GCHP}">:-convert native>
+        $<NOT:$<BOOL:"${GCHP}">>:-convert big_endian>
+        -fp-model source -mcmodel=medium
         -shared-intel -traceback -DLINUX_IFORT
     )
     set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
     set(CMAKE_Fortran_FLAGS_DEBUG "-g -O0 -check arg_temp_created -debug all -DDEBUG")
 elseif("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "GNU")
     target_compile_options(BaseTarget INTERFACE
-        -cpp -w -std=legacy -fautomatic -fno-align-commons -fconvert=big-endian 
+        -cpp -w -std=legacy -fautomatic -fno-align-commons 
+        $<$<BOOL:"${GCHP}">:-fconvert=native>
+        $<NOT:$<BOOL:"${GCHP}">>:-fconvert=big-endian>
         -fno-range-check -mcmodel=medium -fbacktrace -g -DLINUX_GFORTRAN
     )
     set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -funroll-loops")
